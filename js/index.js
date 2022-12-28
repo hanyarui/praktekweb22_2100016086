63a2bdeb3217d1d17281586de78d04c2eb549a01
Dharu Aulia Rahman <106579507+ruinzyu@users.noreply.github.com>
2022-12-28 23:59:49 +0700

    first commit

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function myFunction() {
 
     if (username == "coba" && password == "tes") {
         alert("You are a valid user");
-        window.open("homepage.html");
+        window.open("homepage.html", "_blank").focus();
     } else {
         alert("You are not a valid user");
     }
